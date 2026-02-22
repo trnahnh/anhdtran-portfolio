26a0bdf82365692e6b465b3f31d8c711c4b2f6a4
Anh D Tran <tran3ah@mail.uc.edu>
2026-02-22 19:31:57 +0700

    Sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -23,5 +23,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.7,
     },
+    {
+      url: `${baseUrl}/terminal`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/space`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
   ];
 }
